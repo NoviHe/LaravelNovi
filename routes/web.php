56e9fa8c3cb4bb3f205aef5db02ad4abd028a00f
NoviHe <claywhoami6@gmail.com>
2019-02-17 17:27:55 +0700

    tugas video 10

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ route::group(['middleware'=>['auth']], function(){
 			return view('admin.pages.dashboard'); 
 		})->name('admin.home');
 	route::prefix('user')->group(function(){
+		route::get('/','UserController@daftar')->name('admin.user');
 		route::get('/setting','UserSettingController@form')->name('admin.user.setting');
 		route::post('/setting','UserSettingController@update');
 		});
