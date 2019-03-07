69af777f14ae7f4094591e29709fd74b6ff24767
NoviHe <claywhoami6@gmail.com>
2019-03-07 20:04:10 +0700

    Tugas 26

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -43,6 +43,10 @@ class KategoriController extends Controller
 
     public function update(request $req)
     {
+    	\Validator::make($req->all(),[
+    		'kategori'=>'required|between:3,100|unique:kategori,nama_kategori,'.$req->id,
+    	])->validate();
+
     	return 'fungsi Update';
     }
 }
