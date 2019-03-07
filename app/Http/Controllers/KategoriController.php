fb79912b96a597b99de562076a1b94993047299d
NoviHe <claywhoami6@gmail.com>
2019-03-07 20:12:06 +0700

    Tugas 27

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -47,6 +47,11 @@ class KategoriController extends Controller
     		'kategori'=>'required|between:3,100|unique:kategori,nama_kategori,'.$req->id,
     	])->validate();
 
-    	return 'fungsi Update';
+    	$result = Kategori::where('id',$req->id)->update(['nama_kategori'=>$req->kategori,]);
+    	if ( $result ) {
+    		return redirect()->route('admin.kategori')->with('result','update');
+    	} else {
+    		return back()->with('result','fail');
+    	}
     }
 }
